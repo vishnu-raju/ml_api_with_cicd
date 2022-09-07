f1b4cd2af1cd4f9270aafebf41acaab73790e2dc
Vishnu N Raju <vishstud@gmail.com>
2022-09-07 12:33:21 +0000

    setting up cicd

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://<yourappname>.azurewebsites.net:$PORT/predict 
+     -X POST https://<yourappname>:$PORT/predict 
      #your application name <yourappname>goes here
